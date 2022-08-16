5caa697d47278a3579a3936e39a3519be62ea40e
Katerina <katerina4484@mail.ru>
2022-08-16 22:59:26 +0500

    ex19

--- ex19/Program.cs
+++ ex19/Program.cs
@@ -3,3 +3,35 @@
 12821 -> да
 23432 -> да */
 
+int number = ReadInt("Введите пятизначное число: ");
+int num2 = number/1000;
+int num4 = number/10;
+
+if (number>9999 && number<100000)
+
+{if (number/10000 == Mod(number) && Mod(num2) == Mod(num4))
+{
+    Console.WriteLine(number + "-> да");
+}
+else
+{
+    Console.WriteLine(number + "-> нет");
+}
+}
+
+else
+{
+    Console.WriteLine("Введено не пятизначное число");
+}
+
+int ReadInt(string message)
+{
+     Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Mod(int num)
+{
+    return num%10;
+}
+
